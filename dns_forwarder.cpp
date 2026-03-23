8fd4d784fa814d4b94ce6aa686b3c465ae5d0418
Abdullah Zidan <abdullahzidan62@gmail.com>
2026-03-23 19:31:16 -0400

    feat: create dns forwarder to google public dns and send response back to client

---
+++ dns_forwarder.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 6464
+#define DNS_RESOLVER_PORT 53
+#define DNS_RES_IP "8.8.8.8"
+
+int main() {
+	int fd_client_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	if (fd_client_socket == -1) {
+		perror("Error occurred with socket()");
+		return 1;
+	}
+
+	int fd_rsolvr_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	if (fd_rsolvr_socket == -1) {
+		perror("Error occurred with socket()");
+		return 1;
+	}
+
+	struct sockaddr_in server_addr;
+	socklen_t server_len = sizeof(server_addr);
+	memset(&server_addr, 0, server_len);
+	server_addr.sin_port = htons(PORT);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+
+	if (bind(fd_client_socket, (struct sockaddr*)&server_addr, server_len) == -1) {
+		perror("Error occurred with bind()");
+		return 1;
+	}
+
+	std::cout << "Listening on port " << PORT << "...\n";
+
+
+	uint8_t buf[512];
+
+	const char* dns_resolver_ip = DNS_RES_IP;
+	struct in_addr ipv4_binary;
+
+	if (inet_pton(AF_INET, dns_resolver_ip, &ipv4_binary) == -1) {
+		perror("Error occurred with inet_pton()");
+		return 1;
+	}
+
+	struct sockaddr_in rsolvr_addr;
+	socklen_t rsolvr_len = sizeof(rsolvr_addr);
+	memset(&rsolvr_addr, 0, rsolvr_len);
+	rsolvr_addr.sin_port = htons(DNS_RESOLVER_PORT);
+	rsolvr_addr.sin_family = AF_INET;
+	rsolvr_addr.sin_addr.s_addr = ipv4_binary.s_addr;
+
+	while (true) {
+		struct sockaddr_in client_addr;
+		socklen_t client_len = sizeof(client_addr);
+
+		ssize_t n = recvfrom(fd_client_socket, buf, 512, 0, (struct sockaddr*) &client_addr, (socklen_t*) &client_len);
+		if (n == -1) {
+			perror("Error occurred with recvfrom()");
+			break;
+		}
+
+		std::cout << "Received " << n << " bytes\n";
+
+		if (sendto(fd_rsolvr_socket, buf, n, 0, (struct sockaddr*) &rsolvr_addr, rsolvr_len) == -1) {
+			perror("Error occurred with sendto()");
+			break;
+		}
+
+		ssize_t dns_resolver_response = recvfrom(fd_rsolvr_socket, buf, 512, 0, (struct sockaddr*) &rsolvr_addr, (socklen_t*) &rsolvr_len);
+		if (dns_resolver_response == -1) {
+			perror("Error occurred with recvfrom()");
+			break;
+		}
+
+		if (sendto(fd_client_socket, buf, dns_resolver_response, 0, (struct sockaddr*) &client_addr, client_len) == -1) {
+			perror("Error occurred with sendto()");
+			break;
+		}
+	}
+	if (close(fd_client_socket) == -1) {
+		perror("Error occurred with close()");
+		return 1;
+	}
+
+	return 0;
+}
