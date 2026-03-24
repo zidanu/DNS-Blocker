9a086f4b332de7f50126aa712affe03b93988623
Abdullah Zidan <abdullahzidan62@gmail.com>
2026-03-24 19:11:36 -0400

    fix: update if else conditions so first label length byte is not added to domain string

--- packet_parser.cpp
+++ packet_parser.cpp
@@ -1,16 +1,15 @@
 #include <cstdint>
 #include <string>
-#include <unistd.h>
 
 
-std::string parse_domain(const uint8_t buf[], int offset) {
+std::string parse_domain_from_query(const uint8_t buf[], int offset) {
 	std::string domain;
 	int i = offset;
 	while (buf[i] != '\0') {
 		// <= 63 because max length of top-level domains is 63
 		if (i > offset && buf[i] <= 63) {
 			domain.push_back('.');
-		} else {
+		} else if (i > offset) {
 			domain.push_back(buf[i]);
 		}
 		i++;
