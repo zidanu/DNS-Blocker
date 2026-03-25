b37032b27e51f7cf0e168f3c2565d5d5dd237587
Abdullah Zidan <abdullahzidan62@gmail.com>
2026-03-25 01:58:54 -0400

    refactor: create temporary block_list and intercept domains in that list using deny_domain from packet_parser.cpp

--- dns_forwarder.cpp
+++ dns_forwarder.cpp
@@ -4,6 +4,9 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <unordered_set>
+
+#include "packet_parser.cpp"
 
 #define PORT 6464
 #define DNS_RESOLVER_PORT 53
@@ -54,9 +57,14 @@ int main() {
 	rsolvr_addr.sin_family = AF_INET;
 	rsolvr_addr.sin_addr.s_addr = ipv4_binary.s_addr;
 
+	std::unordered_set<std::string> block_list = {"google.com", "abc.com"};
+
 	while (true) {
 		struct sockaddr_in client_addr;
 		socklen_t client_len = sizeof(client_addr);
+
+		struct sockaddr_in client2_addr;;
+		socklen_t client2_len = sizeof(client2_addr);
 
 		ssize_t n = recvfrom(fd_client_socket, buf, 512, 0, (struct sockaddr*) &client_addr, (socklen_t*) &client_len);
 		if (n == -1) {
@@ -66,12 +74,23 @@ int main() {
 
 		std::cout << "Received " << n << " bytes\n";
 
+		std::string domain = parse_domain_from_query(buf, 12);
+		if (block_list.find(domain) != block_list.end()) {
+			int cutoff = deny_domain(buf);
+			std::cout << cutoff << '\n';
+			if (sendto(fd_client_socket, buf, cutoff, 0, (struct sockaddr*) &client_addr, client_len) == -1) {
+				perror("Error occurred with sendto()");
+				break;
+			}
+			continue;
+		}
+
 		if (sendto(fd_rsolvr_socket, buf, n, 0, (struct sockaddr*) &rsolvr_addr, rsolvr_len) == -1) {
 			perror("Error occurred with sendto()");
 			break;
 		}
 
-		ssize_t dns_resolver_response = recvfrom(fd_rsolvr_socket, buf, 512, 0, (struct sockaddr*) &rsolvr_addr, (socklen_t*) &rsolvr_len);
+		ssize_t dns_resolver_response = recvfrom(fd_rsolvr_socket, buf, 512, 0, (struct sockaddr*) &client2_addr, (socklen_t*) &client2_len);
 		if (dns_resolver_response == -1) {
 			perror("Error occurred with recvfrom()");
 			break;
